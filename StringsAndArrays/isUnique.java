33e340cfa741e08a57964dc7ea0f015bbee39d62
deepti15992 <deepti.venkatachalam@gmail.com>
2017-07-09 16:01:55 -0700

    Update isUnique.java

--- StringsAndArrays/isUnique.java
+++ StringsAndArrays/isUnique.java
@@ -1,5 +1,5 @@
 public Boolean isUnique(String str){
-	BitSet bitSet = new BitSet(27); //change to represent casesensitive alphanumeric etc
+	BitSet bitSet = new BitSet(26); //change to represent casesensitive alphanumeric etc
 	char a = 'a';
 	int ascii = (int) a;
 	for(int i = 0; i<str.size(); i++){
@@ -9,5 +9,20 @@ public Boolean isUnique(String str){
 		else
 			bitSet.set(value);
 	} 
+	return true;
+}
+
+//O(N)
+
+//with bitVector
+
+public Boolean isUnique(String str){
+	int checker = 0;
+	for(int i = 0; i < str.length(); i++){
+		int value = str.charAt(i) - 'a';
+		if((checker & (1 << value)) > 0)
+			return false;
+		checker = checker | (1 << value);
+	}
 	return true;
 }
